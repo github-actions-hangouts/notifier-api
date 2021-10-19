d649d3f0270b3463b3521b5bf875636457f00d4c
lcunha <lucas.cunha@capgemini.com>
2021-10-19 15:06:11 -0300

    Add NATS test workflows

--- .github/scripts/startApi.ps1
+++ .github/scripts/startApi.ps1
@@ -1,9 +1,13 @@
 param (
     [Parameter()]
+    [string]$BasePath,
+
+    [Parameter()]
     [string]$ApiName
 )
 
 $ErrorActionPreference = "Stop"
 
 $Env:ASPNETCORE_URLS="http://0.0.0.0:5000/"
-dotnet "./app/$ApiName.dll"
+$api = Get-ChildItem $BasePath/app -Recurse | Where Name -match "$ApiName.dll"
+dotnet $api.FullName
